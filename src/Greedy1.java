eb7471c05c3d48d3f9f10e13486e3b69dddfa345
kjh <kjh4940@naver.com>
2024-04-08 19:03:29 +0900

    greedy

---
+++ src/Greedy1.java
@@ -0,0 +1,23 @@
+public class Greedy1 {
+
+    public static void main(String[] args) {
+
+        System.out.println(greedy(17, 4));
+    }
+
+    public static int greedy(int N, int K) {
+
+        int answer = 0;
+
+        while (N != 1) {
+            if (N % K == 0) {
+                N /= K;
+            } else {
+                N += - 1;
+            }
+            answer++;
+        }
+
+        return answer;
+    }
+}
---
+++ src/Greedy2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Greedy2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String number = sc.nextLine();
+
+        int result = number.charAt(0) - '0';
+        for (int i = 1; i < number.length(); i++) {
+            int num = number.charAt(i) - '0';
+            System.out.println(num);
+            if (num <= 1 || result <= 1) {
+                result += num;
+            } else {
+                result *= num;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
